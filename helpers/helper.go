52c05a19557dc77abc76b3813fdd789f9c4b8a87
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2026-07-11 07:49:28 +0530

    helpers: add GetLogger() to IRabbitConnection interface

--- helpers/helper.go
+++ helpers/helper.go
@@ -6,9 +6,11 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 
 	"github.com/Srajan-Sanjay-Saxena/goRabbit-axon/channel"
+	"github.com/Srajan-Sanjay-Saxena/goRabbit-axon/logger"
 )
 
 type IRabbitConnection interface {
 	GetChannel(ctx context.Context, onClose channel.OnChannelClose) (*amqp.Channel, error)
+	GetLogger() *logger.Logger
 	Shutdown() error
 }
