338af94e77ed61a85fc16126353e836f13070b88
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2026-07-11 07:49:28 +0530

    exchange: refactor NewRabbitExchange to accept RabbitExchangeConfig

--- exchange/exhchange.go
+++ exchange/exhchange.go
@@ -31,31 +31,26 @@ func (et ExchangeTopic) String() string {
 }
 
 type RabbitExchangeClass struct {
-	ExchangeName    string
-	exchangeType    ExchangeTopic
-	exchangeOptions RabbitExchangeOptions
+	config RabbitExchangeConfig
 }
 
-func NewRabbitExchange(exchangeName string, exchangeType ExchangeTopic, exchangeOptions RabbitExchangeOptions) *RabbitExchangeClass {
-	return &RabbitExchangeClass{
-		ExchangeName:    exchangeName,
-		exchangeType:    exchangeType,
-		exchangeOptions: exchangeOptions,
-	}
+func NewRabbitExchange(cfg RabbitExchangeConfig) *RabbitExchangeClass {
+	return &RabbitExchangeClass{config: cfg}
 }
 
 func (rbEx *RabbitExchangeClass) CreateExchange(ctx context.Context, conn helpers.IRabbitConnection) error {
-	ch, err := conn.GetChannel(ctx , nil)
+	ch, err := conn.GetChannel(ctx, nil)
 	if err != nil {
 		return err
 	}
 	defer ch.Close()
 
-	return ch.ExchangeDeclare(rbEx.ExchangeName, rbEx.exchangeType.String(), rbEx.exchangeOptions.Durable, rbEx.exchangeOptions.AutoDelete, rbEx.exchangeOptions.Internal, rbEx.exchangeOptions.NoWait, nil)
+	c := rbEx.config
+	return ch.ExchangeDeclare(c.Name, c.Type.String(), c.Durable, c.AutoDelete, c.Internal, c.NoWait, nil)
 }
 
 func (rbEx *RabbitExchangeClass) CreateQueue(ctx context.Context, conn helpers.IRabbitConnection, cfg RabbitQueueConfig) (amqp.Queue, error) {
-	ch, err := conn.GetChannel(ctx , nil)
+	ch, err := conn.GetChannel(ctx, nil)
 	if err != nil {
 		return amqp.Queue{}, err
 	}
@@ -74,7 +69,7 @@ func (rbEx *RabbitExchangeClass) CreateQueue(ctx context.Context, conn helpers.I
 		return amqp.Queue{}, err
 	}
 
-	if err := ch.QueueBind(q.Name, cfg.BindingKey, rbEx.ExchangeName, cfg.NoWait, nil); err != nil {
+	if err := ch.QueueBind(q.Name, cfg.BindingKey, rbEx.config.Name, cfg.NoWait, nil); err != nil {
 		return amqp.Queue{}, err
 	}
 
